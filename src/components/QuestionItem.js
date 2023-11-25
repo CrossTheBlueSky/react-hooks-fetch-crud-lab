bba0e0f2afb5c7206993a956da5e199356a28872
Derek Mercedes <mercedes.derek@gmail.com>
2023-11-25 13:59:00 -0700

    deliverables met

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, onUpdate }) {
   const { id, prompt, answers, correctIndex } = question;
-
+  
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
+
+  function deleteClickHandler(){
+    onDelete(id)
+  }
+
+  function changeHandler(e){
+    question.correctIndex = e.target.value
+    onUpdate(question)
+  }
 
   return (
     <li>
@@ -15,9 +24,9 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={changeHandler}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={deleteClickHandler}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+
+const [questions, setQuestions] = useState([])
+
+useEffect(fetchData, [])
+
+function fetchData(){
+
+  fetch("http://localhost:4000/questions")
+  .then(r=>r.json())
+  .then(data=>{
+
+    setQuestions(data.map((question)=>{
+      return (
+        <QuestionItem key={question.id} question={question} onDelete={handleDelete} onUpdate={updateHandler}/>
+      )
+
+   }))
+  })
+}
+
+function handleDelete(id){
+  fetch(`http://localhost:4000/questions/${id}`, {
+    method: "DELETE"
+  }).then(fetchData)
+
+}
+
+function updateHandler(item){
+  console.log(item)
+  item.correctIndex = parseInt(item.correctIndex)
+ 
+  fetch(`http://localhost:4000/questions/${item.id}`, {
+    method: "PATCH",
+    headers: {"Content-Type" : "application/json"},
+    body: JSON.stringify(item)
+  })
+  .then(fetchData)
+}
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions}</ul>
     </section>
   );
 }
